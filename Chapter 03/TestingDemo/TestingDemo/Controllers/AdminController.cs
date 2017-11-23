92503bbd777a863fbf1bff7008e669d5877f6f82
tiger02139 <sander414@gmail.com>
2017-11-22 23:14:36 -0500

    minor modifications

--- Chapter 03/TestingDemo/TestingDemo/Controllers/AdminController.cs
+++ Chapter 03/TestingDemo/TestingDemo/Controllers/AdminController.cs
@@ -1,12 +1,24 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 
 namespace TestingDemo {
 
     public class AdminController : Controller {
         private IUserRepository repository;
 
+
         public AdminController(IUserRepository repo) {
             repository = repo;
+        }
+
+        /// <summary>
+        /// Apparently since a constructor requiring a parameter has been created,
+        /// C# no longer supplies an implied parameterless constructor, so we have
+        /// to supply one ourself.
+        /// https://www.codeproject.com/Questions/1088244/Get-error-is-no-parameterless-constructor-defined
+        /// </summary>
+        public AdminController()
+        {
         }
 
         public ActionResult ChangeLoginName(string oldName, string newName) {
@@ -14,6 +26,13 @@ namespace TestingDemo {
             user.LoginName = newName;
             repository.SubmitChanges();
             // render some view to show the result
+            return View();
+        }
+
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View();
         }
     }
