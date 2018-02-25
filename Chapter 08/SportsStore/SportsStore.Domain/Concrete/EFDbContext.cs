805b5114f2502cc7787f76a2ab4ddd127f06e866
tiger02139 <sander414@gmail.com>
2018-02-24 23:34:56 -0500

    change connection string name and EFDbContext

--- Chapter 08/SportsStore/SportsStore.Domain/Concrete/EFDbContext.cs
+++ Chapter 08/SportsStore/SportsStore.Domain/Concrete/EFDbContext.cs
@@ -3,7 +3,12 @@ using System.Data.Entity;
 
 namespace SportsStore.Domain.Concrete {
 
-    public class EFDbContext : DbContext {
+    public class EFDbContext : DbContext
+    {
+        public EFDbContext() : base("EFDbContextConnectionString")
+        {
+        }
+
         public DbSet<Product> Products { get; set; }
     }
 }
